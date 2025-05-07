ad1603b2b2af20d5694ba0b75707a2f784c5d1e9
RakibHossen678 <rh491464@gmail.com>
2025-05-07 20:56:29 +0600

    Solved Problem 3 and 4

--- solutions.ts
+++ solutions.ts
@@ -8,3 +8,55 @@ function formatString(input: string, toUpper?: boolean): string {
 console.log(formatString("Hello"));
 console.log(formatString("Hello", true));
 console.log(formatString("Hello", false));
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const booksWithRatingsMoreThan4 = items.filter((item) => item.rating >= 4);
+  return booksWithRatingsMoreThan4;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+console.log(filterByRating(books));
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
+
+console.log(concatenateArrays(["a", "b"], ["c"]));
+console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+class Vihicale {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    console.log(`Make: ${this.make}, Model: ${this.year}`);
+  }
+}
+
+class Car extends Vihicale {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(`Model: ${this.model}`);
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();
+myCar.getModel();
